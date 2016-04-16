784fd1a383619c8cecc0730d5e81c1df10e2f30b
zhouguang <zhouguang@kenrobot.com>
2016-04-17 07:17:31 +0800

    utf-8

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,7 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-		<!-- <meta charset='utf-8'> -->
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta charset='utf-8'>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<title>啃萝卜智能硬件平台</title>
