fd6395518895d4835c470b43fdcb3fd489458306
kite17 <wangkunpeng@kenrobot.com>
2016-05-05 14:43:19 +0800

    relase

--- public/assets/js/ng/controller/alerts.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-angular.module('kenrobot')
-	.controller('AlertsCtrl', function($scope, alertsService) {
-		$scope.alerts = alertsService.getInstance();
-		$scope.generateSvgUrl = function(id) {
-			return '/assets/images/sprite.svg#' + id;
-		};
-	});
--- public/assets/js/ng/factory/lodash.factory.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-angular.module('kenrobot')
-	.factory('_', function($window) {
-		return $window._;
-	});
