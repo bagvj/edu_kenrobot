71078f9ed876a8bc984834ceab2e075185ea7750
zhouguang <zhouguang@kenrobot.com>
2016-05-24 13:56:48 +0800

    update crx

--- app/Extension/dist/background.js
+++ app/Extension/dist/background.js
@@ -1,18 +1,1 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-	chrome.app.window.create('index.html', {
-		'resizable': false,
-		'bounds': {
-			'width': 670,
-			'height': 440,
-		}
-	});
-});
-
-chrome.runtime.onMessageExternal.addListener(function(message, sender, sendResponse) {
-	if(message && message == "ping") {
-		sendResponse({
-			action: "ping",
-			result: "pong",
-		});
-	}
-});
+chrome.app.runtime.onLaunched.addListener(function(){chrome.app.window.create("index.html",{resizable:!1,bounds:{width:670,height:440}})}),chrome.runtime.onMessageExternal.addListener(function(e,n,i){e&&"ping"==e&&i({action:"ping",result:"pong"})});
