c29f23771ac24c06e62b98c306fff6e6ed6bbec1
zhouguang <zhouguang@kenrobot.com>
2016-10-14 08:35:23 +0800

    report

--- resources/assets/js/app/component/content/project.js
+++ resources/assets/js/app/component/content/project.js
@@ -80,28 +80,21 @@ define(['vendor/jquery', 'app/util/emitor', 'app/util/util', 'app/model/userMode
 	}
 
 	function onNewClick(e) {
-		// userModel.authCheck(true).done(function() {
-		// 	emitor.trigger('project', 'show');
-		// });
-		throw new Error("test error");
+		userModel.authCheck(true).done(function() {
+			emitor.trigger('project', 'show');
+		});
 	}
 
 	function onSaveClick(e) {
-		// userModel.authCheck(true).done(function() {
-		// 	emitor.trigger('project', 'save', null, 'save');
-		// });
-
-		var a = {};
-		var b = a.name;
-		var c = b.split(" ");
-		console.log(b, c);
+		userModel.authCheck(true).done(function() {
+			emitor.trigger('project', 'save', null, 'save');
+		});
 	}
 
 	function onUploadClick(e) {
-		// userModel.authCheck(true).done(function() {
-		// 	emitor.trigger('project', 'upload');
-		// });
-		emitor.trigger("app", "error", "test error", "project.js", 96, 10);
+		userModel.authCheck(true).done(function() {
+			emitor.trigger('project', 'upload');
+		});
 	}
 
 	function onShowBoardSelect(e) {
