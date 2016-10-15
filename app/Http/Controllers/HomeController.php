ea5ba26d1c2d19d09280ce9b0186deab0348d128
kite17 <wangkunpeng@kenrobot.com>
2016-10-15 20:36:11 +0800

    wechat webauth

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,6 +11,7 @@ use ZipArchive;
 use Curl\Curl;
 use Session;
 use App\Util\Tools;
+use App\WebAuth\Factory as WebAuthFactory;
 
 class HomeController extends Controller {
 
@@ -22,7 +23,20 @@ class HomeController extends Controller {
 	public function index(Request $request) {
 		if (Auth::check()) {
 			$user = Auth::user();
+		} else {
+			$openid = $request->input('openid');
+			if (!empty($openid)) {
+				$webauth = WebAuthFactory::create('weixinweb');
+				$crendentials = compact('openid');
+				$loginResult = $webauth->validate($crendentials);
+
+		        if ($loginResult === true) {
+			        $user = $webauth->localUser();
+			        Auth::login($user, true);
+		        }
+			}
 		}
+
 
 		$loginInfo = Tools::getLoginInfo($request->url());
 		Session::put('key', $loginInfo->key);
