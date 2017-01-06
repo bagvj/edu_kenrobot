54afbae8168b7f9de5fc8dc456572983d891ec6b
wangkunpeng <wangkuneng@kenrobot.com>
2017-01-07 01:21:31 +0800

    保存项目

--- app/WebAuth/UserService.php
+++ app/WebAuth/UserService.php
@@ -13,7 +13,7 @@ class UserService
 		$user = [
                 'name' => isset($userData['base_name']) ? $userData['base_name'] : '',
                 'avatar_url' => isset($userData['base_avatar']) ? $userData['base_avatar'] : '',
-                'uid' => isset($userData['uid']) ? $userData['uid'] : 0,
+                'uid' => isset($userData['user_id']) ? $userData['user_id'] : 0,
                 'user_id' => $userData['user_id'],
         ];
 
